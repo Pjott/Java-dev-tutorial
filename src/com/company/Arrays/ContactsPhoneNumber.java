6ad37ded05478cb1b1a834c8c6367c8bc2692b19
Piotr Bala <piotr.baa@gmail.com>
2017-02-22 20:14:49 +0100

    Added ContactsPhoneNumber class

---
+++ src/com/company/Arrays/ContactsPhoneNumber.java
@@ -0,0 +1,33 @@
+package com.company.Arrays;
+
+import java.util.ArrayList;
+
+/**
+ * Created by piotr on 06.02.17.
+ */
+public class ContactsPhoneNumber {
+
+    //tworzę ArrayListę z nu,eramitelefonow.
+    private ArrayList<Integer> contactsPhonenNumber = new ArrayList<>();
+
+    //getter
+    public ArrayList<Integer> getContactsPhonenNumber() {
+        return contactsPhonenNumber;
+    }
+
+    //dodawanie imion do kontaktów
+    public void addContactsPhonenumber(int number) {
+        contactsPhonenNumber.add(number);
+    }
+
+    //wyswietanie zawartosci
+    public void showContactsPhoneNumber() {
+
+        for (int i = 0; i < contactsPhonenNumber.size(); i ++) {
+            System.out.println((i+1) + ". " + contactsPhonenNumber.get(i));
+        }
+    }
+
+    //ArrayList<String> blabla = new ArrayList<>();
+
+}
